dd4d6eeab05b2868a774e99866a6216221d3c607
AdaHe <168495114+aAdaHe@users.noreply.github.com>
2025-06-03 09:49:30 +0800

    Update 14.user_add_form.php

--- 14.user_add_form.php
+++ 14.user_add_form.php
@@ -2,13 +2,17 @@
     <head><title>新增使用者</title></head>
     <body>
 <?php        
-    error_reporting(0);
-    session_start();
+    error_reporting(0);   // 關閉錯誤報告（不建議用於開發環境，可能會忽略重要錯誤）
+    session_start();  // 啟動 session 功能，用來存取使用者的登入資訊
+
+    // 檢查是否已登入
     if (!$_SESSION["id"]) {
+         // 若未登入，顯示訊息, 3 秒後導向登入 "2.login.html" 頁面
         echo "請登入帳號";
         echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
     }
     else{    
+        // 若已登入，顯示新增使用者的表單
         echo "
             <form action=15.user_add.php method=post>
                 帳號：<input type=text name=id><br>
